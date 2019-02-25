312368088c97a4eba2aedde3ba266dda979fa4be
franjvega <47573373+franjvega@users.noreply.github.com>
2019-02-25 21:40:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import unittest
+import cv2 as cv
 
 class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_CamIn(self):
+        self.assertTrue(functions.CameraIn("video.avi"))
+        
+        
+    def test_OutStore(self):
+        self.assertTrue(functions.In_Store(10, 480, 70, 100))
+        self.assertFalse(functions.In_Store(100, 480, 400, 1000))
+    def test_Detect(self):
+        imgBW = cv.imread("image_background.jpg")
+        imgNew = cv.imread("image_new.jpg")
+        self.assertTrue(functions.Detect(imgBW, imgNew))

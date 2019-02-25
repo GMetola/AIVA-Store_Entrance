e328aa9fb81c5d8c79efd270d79885fd50a3f8ad
franjvega <47573373+franjvega@users.noreply.github.com>
2019-02-25 21:40:20 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import cv2 as cv
 
 class TestStringMethods(unittest.TestCase):
     def test_CamIn(self):
-        self.assertTrue(functions.CameraIn("video.avi"))
-        
-        
+        self.assertTrue(functions.CameraIn("video.avi"))     
+      
     def test_OutStore(self):
         self.assertTrue(functions.In_Store(10, 480, 70, 100))
         self.assertFalse(functions.In_Store(100, 480, 400, 1000))
+        
     def test_Detect(self):
         imgBW = cv.imread("image_background.jpg")
         imgNew = cv.imread("image_new.jpg")

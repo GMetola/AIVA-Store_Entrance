2427fce94f47e6ace46f4ae889d47e9252d3a461
GMetola <47193318+GMetola@users.noreply.github.com>
2019-04-10 00:13:45 +0200

    Update test.py

--- test.py
+++ test.py
@@ -12,30 +12,12 @@ class TestStringMethods(unittest.TestCase):
 		""" Test that the cv2 module can be imported. """
 		import cv2 as cv
 
-	def test_detect(self):
-		import cv2 as cv
-		self.assertTrue(FindAndFollow.detect(image=cv.imread("./test_detected.jpg")))
-
-	# def test_initialization(self):
-	#     self.assertEqual(FindAndFollow.follow(), 0)
-	#     print("alright")
-
 	def test_video_capture(self):
 
 		import cv2 as cv
 		cap = cv.VideoCapture("EnterExitCrossingPaths1front.mpg")
 		self.assertTrue(cap.isOpened())
 
-	def test_videoin(self):
-		import cv2 as cv
-		viddata = Classes.VideoData()
-		viddata.videoin()
-		cap = cv.VideoCapture("EnterExitCrossingPaths1front.mpg")
-		self.assertTrue(cap.isOpened())
 		
-	def test_follow(self):
-		self.assertEqual(FindAndFollow.follow(self, 0), [0,0])
-
-
 if __name__ == '__main__':
     unittest.main()
